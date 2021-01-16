674aef3829b2779b288e75a4c1fca6df8874b0aa
John Beresford <jberesford@volcaus.com>
2021-01-15 18:10:46 -0800

    carousel work

---
+++ src/components/carousel/carouselImages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CarouselImages = () => {
+  return (
+    <aside className={styles.carouselContent}>
+      
+    </aside>
+  )
+}
+
+export default CarouselImages
