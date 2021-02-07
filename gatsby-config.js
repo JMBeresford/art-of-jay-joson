1734f30d845b75cda5f7bcec610ce859b3a0cb96
John Beresford <jberesford@volcaus.com>
2021-02-06 20:33:52 -0800

    sal set animations to run once only

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
             resolve: `gatsby-plugin-scroll-reveal`,
             options: {
                 threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
-                once: false, // Defines if animation needs to be launched once
+                once: true, // Defines if animation needs to be launched once
                 disable: false, // Flag for disabling animations
                 
                 // Advanced Options
