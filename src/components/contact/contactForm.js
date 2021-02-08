edc953080acf597a00cf17dc6606d455891bb279
John Beresford <jberesford@volcaus.com>
2021-02-07 20:56:14 -0800

    form update

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import { navigate } from 'gatsby'
+import React, { useState } from 'react'
 import styles from './contactForm.module.css'
 
 const ContactForm = () => {
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: ""
+  })
+
+  function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: [e.target.value]})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const form = e.target
+
+    fetch('/', {
+      method: "POST",
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...formState,
+      }),
+    })
+    .then(() => alert("Success!"))
+    .catch(error => alert(error));
+  }
 
   return (
     <div className={styles.formWrapper}>
@@ -9,28 +44,28 @@ const ContactForm = () => {
       <form
         id="contactForm"
         name="contact"
-        method="POST"
+        method="post"
         data-netlify="true"
-        data-netlify-recaptcha="true"
-        netlify-honeypot="bot-field"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
       >
 
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
 
         <label htmlFor="name">Name</label>
-        <input name="name" type="text"></input>
+        <input name="name" type="text" onChange={handleChange} />
         
         <label htmlFor="email">Email</label>
-        <input name="email" type="text"></input>
+        <input name="email" type="text" onChange={handleChange} />
         
         <label htmlFor="phone">Phone</label>
-        <input name="phone" type="text"></input>
+        <input name="phone" type="text" onChange={handleChange} />
 
         <label htmlFor="message">Message</label>
-        <textarea name="message"></textarea>
+        <textarea name="message" onChange={handleChange} />
 
-        <div id="captcha" className={styles.captcha} data-netlify-recaptcha="true"></div>
+        <div id="captcha" className={styles.captcha} data-netlify-recaptcha="true" />
 
         <button type="submit">Let's Talk!</button>
         
