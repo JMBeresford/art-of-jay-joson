fcbb0887d9bd7dcb86c7c29d22cd7caed07d26da
John Beresford <jberesford@volcaus.com>
2021-02-24 23:08:28 -0800

    gallery bugfix

--- src/components/gallery/galleryCategories.js
+++ src/components/gallery/galleryCategories.js
@@ -3,32 +3,34 @@ import styles from './galleryCategories.module.css'
 
 const GalleryCategories = (props) => {
   return (
-    <nav className={styles.wrapper}>
-      <div className={styles.mobileDashes}>
-        <div className={styles.fader1} />
-        <p>—</p>
-        <p>—</p>
-        <div className={styles.fader2} />
-      </div>
-      <div className={styles.categories}>
-        <button href="#"
-          onClick={() => props.changeCategory(0)}
-          className={props.category === 0 ? '' : styles.inactive}
-        >Illustrations</button>
-        <button href="#"
-          onClick={() => props.changeCategory(1)}
-          className={props.category === 1 ? '' : styles.inactive}
-        >Animations</button>
-        <button href="#"
-          onClick={() => props.changeCategory(2)}
-          className={props.category === 2 ? '' : styles.inactive}
-        >Character Designs</button>
-        <button href="#"
-          onClick={() => props.changeCategory(3)}
-          className={props.category === 3 ? '' : styles.inactive}
-        >Story Boards</button>
-      </div>
-    </nav>
+    <div className={styles.container}>
+      <nav className={styles.wrapper}>
+        <div className={styles.mobileDashes}>
+          <div className={styles.fader1} />
+          <p>—</p>
+          <p>—</p>
+          <div className={styles.fader2} />
+        </div>
+        <div className={styles.categories}>
+          <button href="#"
+            onClick={() => props.changeCategory(0)}
+            className={props.category === 0 ? '' : styles.inactive}
+          >Illustrations</button>
+          <button href="#"
+            onClick={() => props.changeCategory(1)}
+            className={props.category === 1 ? '' : styles.inactive}
+          >Animations</button>
+          <button href="#"
+            onClick={() => props.changeCategory(2)}
+            className={props.category === 2 ? '' : styles.inactive}
+          >Character Designs</button>
+          <button href="#"
+            onClick={() => props.changeCategory(3)}
+            className={props.category === 3 ? '' : styles.inactive}
+          >Story Boards</button>
+        </div>
+      </nav>
+    </div>
   )
 }
 
